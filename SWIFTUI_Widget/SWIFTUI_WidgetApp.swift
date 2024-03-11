26aed79564dada34c89f0f3662866e2b55086b9e
이윤수 <yslee@iyunsuui-MacBookPro.local>
2024-03-11 17:39:41 +0900

    first commit

---
+++ SWIFTUI_Widget/SWIFTUI_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTUI_WidgetApp.swift
+//  SWIFTUI_Widget
+//
+//  Created by 이윤수 on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUI_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
