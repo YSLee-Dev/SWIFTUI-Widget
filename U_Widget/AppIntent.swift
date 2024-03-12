e04e89fc89a3ca786d3663ed781301fc44ba3a39
이윤수 <yslee@iyunsuui-MacBookPro.local>
2024-03-12 17:28:28 +0900

    Feat(App): 위젯 Target 추가

---
+++ U_Widget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  U_Widget
+//
+//  Created by 이윤수 on 3/12/24.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ U_Widget/U_WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  U_WidgetBundle.swift
+//  U_Widget
+//
+//  Created by 이윤수 on 3/12/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct U_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        U_Widget()
+    }
+}
